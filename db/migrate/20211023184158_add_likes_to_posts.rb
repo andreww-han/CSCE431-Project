ab4a274c18cf7ef28e4783d23df5b68a57340899
andrew-han <andrew.han@tamu.edu>
2021-11-07 18:22:11 -0600

    posts schema adjustments

--- db/migrate/20211023184158_add_likes_to_posts.rb
+++ db/migrate/20211023184158_add_likes_to_posts.rb
@@ -1,4 +1,4 @@
-class AddLikesCountToPosts < ActiveRecord::Migration[6.1]
+class AddLikesToPosts < ActiveRecord::Migration[6.1]
   def change
     add_column :posts, :likes_count, :integer, :default => 0
   end
---
+++ db/migrate/20211108002048_add_numlikes_to_posts.rb
@@ -0,0 +1,5 @@
+class AddNumlikesToPosts < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :numlikes, :integer
+  end
+end
